cfadefac95003059f623ead93772b5b42f634164
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2025-04-25 12:14:31 -0300

    install components by shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
 
+  function handleClick() {
+    alert('Hello World')
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <Button onClick={handleClick}>Adicionar funcionalidade</Button>
+    </div>
   )
 }
