8cf4be9c243aefc41e0bf92d99a7772856811055
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2025-05-15 20:35:24 -0300

    style: add cursor pointer

--- src/components/GoBack/GoBack.tsx
+++ src/components/GoBack/GoBack.tsx
@@ -10,7 +10,7 @@ export const GoBack = ({text = 'Voltar', path = '/'}: GoBackProps) => {
     const navigate = useNavigate();
     
     return (
-        <div onClick={() => navigate(path)} className="inline-flex items-center gap-2 text-gray-600 hover:text-gray-700 cursor-pointer">
+        <div onClick={() => navigate(path)} className="inline-flex items-center gap-2 text-gray-300 hover:text-gray-400 cursor-pointer">
             <ArrowLeft className="w-5 h-5" />
             {text}
         </div>
